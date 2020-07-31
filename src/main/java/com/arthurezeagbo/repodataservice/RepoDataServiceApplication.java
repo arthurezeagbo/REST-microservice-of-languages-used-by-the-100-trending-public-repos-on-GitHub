4adba0e81ec297d763a0967cfd516fa88d01bf0a
unknown <arthurezeagbo@gmail.com>
2020-07-31 14:25:36 +0100

    created and initialized project

---
+++ src/main/java/com/arthurezeagbo/repodataservice/RepoDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arthurezeagbo.repodataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurezeagbo/repodataservice/RepoDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurezeagbo.repodataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
