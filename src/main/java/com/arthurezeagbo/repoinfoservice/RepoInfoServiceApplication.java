6bf52613fbcf7e288fea3057fddf5dfaa998157b
unknown <arthurezeagbo@gmail.com>
2020-07-31 16:10:51 +0100

    created and initialized project

---
+++ src/main/java/com/arthurezeagbo/repoinfoservice/RepoInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arthurezeagbo.repoinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurezeagbo/repoinfoservice/RepoInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurezeagbo.repoinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
