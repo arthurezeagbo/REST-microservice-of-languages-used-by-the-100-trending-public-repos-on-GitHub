325b97d5dda3f626b116f1a5d7f1fac5d96e8213
unknown <arthurezeagbo@gmail.com>
2020-07-31 15:55:46 +0100

    implemented controller endpoint that will make trending repos data available to other micro services

---
+++ src/main/java/com/arthurezeagbo/repodataservice/controllers/RepoController.java
@@ -0,0 +1,33 @@
+package com.arthurezeagbo.repodataservice.controllers;
+
+import com.arthurezeagbo.repodataservice.models.Base;
+import com.arthurezeagbo.repodataservice.models.Item;
+import com.arthurezeagbo.repodataservice.services.IRepoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@RequestMapping("/api/v1/repo")
+@RestController
+public class RepoController {
+
+    @Autowired
+    private IRepoService _repoService;
+
+    @GetMapping("/getall")
+
+    public Flux<Item> getAllData(){
+
+        Flux<Item> data = _repoService.fetchRepositories();
+
+        return data;
+    }
+}
