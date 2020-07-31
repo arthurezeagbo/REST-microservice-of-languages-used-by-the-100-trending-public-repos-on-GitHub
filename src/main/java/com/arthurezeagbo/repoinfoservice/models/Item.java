4eeab6a2fdf4054d24bcc2417a27ab57af8d441f
unknown <arthurezeagbo@gmail.com>
2020-07-31 16:27:46 +0100

    added application models

---
+++ src/main/java/com/arthurezeagbo/repoinfoservice/models/Item.java
@@ -0,0 +1,762 @@
+package com.arthurezeagbo.repoinfoservice.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Item {
+
+    private Integer id;
+    private String nodeId;
+    private String name;
+    private String fullName;
+    private Boolean _private;
+    private Owner owner;
+    private String htmlUrl;
+    private String description;
+    private Boolean fork;
+    private String url;
+    private String forksUrl;
+    private String keysUrl;
+    private String collaboratorsUrl;
+    private String teamsUrl;
+    private String hooksUrl;
+    private String issueEventsUrl;
+    private String eventsUrl;
+    private String assigneesUrl;
+    private String branchesUrl;
+    private String tagsUrl;
+    private String blobsUrl;
+    private String gitTagsUrl;
+    private String gitRefsUrl;
+    private String treesUrl;
+    private String statusesUrl;
+    private String languagesUrl;
+    private String stargazersUrl;
+    private String contributorsUrl;
+    private String subscribersUrl;
+    private String subscriptionUrl;
+    private String commitsUrl;
+    private String gitCommitsUrl;
+    private String commentsUrl;
+    private String issueCommentUrl;
+    private String contentsUrl;
+    private String compareUrl;
+    private String mergesUrl;
+    private String archiveUrl;
+    private String downloadsUrl;
+    private String issuesUrl;
+    private String pullsUrl;
+    private String milestonesUrl;
+    private String notificationsUrl;
+    private String labelsUrl;
+    private String releasesUrl;
+    private String deploymentsUrl;
+    private String createdAt;
+    private String updatedAt;
+    private String pushedAt;
+    private String gitUrl;
+    private String sshUrl;
+    private String cloneUrl;
+    private String svnUrl;
+    private String homepage;
+    private Integer size;
+    private Integer stargazersCount;
+    private Integer watchersCount;
+    private String language;
+    private Boolean hasIssues;
+    private Boolean hasProjects;
+    private Boolean hasDownloads;
+    private Boolean hasWiki;
+    private Boolean hasPages;
+    private Integer forksCount;
+    private Object mirrorUrl;
+    private Boolean archived;
+    private Boolean disabled;
+    private Integer openIssuesCount;
+    private License license;
+    private Integer forks;
+    private Integer openIssues;
+    private Integer watchers;
+    private String defaultBranch;
+    private Double score;
+    //private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public Item(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    @JsonProperty("node_id")
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    @JsonProperty("full_name")
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Boolean getPrivate() {
+        return _private;
+    }
+
+    @JsonProperty("private")
+    public void setPrivate(Boolean _private) {
+        this._private = _private;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    @JsonProperty("owner")
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    @JsonProperty("html_url")
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonProperty("description")
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getFork() {
+        return fork;
+    }
+
+    @JsonProperty("fork")
+    public void setFork(Boolean fork) {
+        this.fork = fork;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("url")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getForksUrl() {
+        return forksUrl;
+    }
+
+    @JsonProperty("forks_url")
+    public void setForksUrl(String forksUrl) {
+        this.forksUrl = forksUrl;
+    }
+
+    public String getKeysUrl() {
+        return keysUrl;
+    }
+
+    @JsonProperty("keys_url")
+    public void setKeysUrl(String keysUrl) {
+        this.keysUrl = keysUrl;
+    }
+
+    public String getCollaboratorsUrl() {
+        return collaboratorsUrl;
+    }
+
+    @JsonProperty("collaborators_url")
+    public void setCollaboratorsUrl(String collaboratorsUrl) {
+        this.collaboratorsUrl = collaboratorsUrl;
+    }
+
+    public String getTeamsUrl() {
+        return teamsUrl;
+    }
+
+    @JsonProperty("teams_url")
+    public void setTeamsUrl(String teamsUrl) {
+        this.teamsUrl = teamsUrl;
+    }
+
+    public String getHooksUrl() {
+        return hooksUrl;
+    }
+
+    @JsonProperty("hooks_url")
+    public void setHooksUrl(String hooksUrl) {
+        this.hooksUrl = hooksUrl;
+    }
+
+    public String getIssueEventsUrl() {
+        return issueEventsUrl;
+    }
+
+    @JsonProperty("issue_events_url")
+    public void setIssueEventsUrl(String issueEventsUrl) {
+        this.issueEventsUrl = issueEventsUrl;
+    }
+
+    public String getEventsUrl() {
+        return eventsUrl;
+    }
+
+    @JsonProperty("events_url")
+    public void setEventsUrl(String eventsUrl) {
+        this.eventsUrl = eventsUrl;
+    }
+
+    public String getAssigneesUrl() {
+        return assigneesUrl;
+    }
+
+    @JsonProperty("assignees_url")
+    public void setAssigneesUrl(String assigneesUrl) {
+        this.assigneesUrl = assigneesUrl;
+    }
+
+    public String getBranchesUrl() {
+        return branchesUrl;
+    }
+
+    @JsonProperty("branches_url")
+    public void setBranchesUrl(String branchesUrl) {
+        this.branchesUrl = branchesUrl;
+    }
+
+    public String getTagsUrl() {
+        return tagsUrl;
+    }
+
+    @JsonProperty("tags_url")
+    public void setTagsUrl(String tagsUrl) {
+        this.tagsUrl = tagsUrl;
+    }
+
+    public String getBlobsUrl() {
+        return blobsUrl;
+    }
+
+    @JsonProperty("blobs_url")
+    public void setBlobsUrl(String blobsUrl) {
+        this.blobsUrl = blobsUrl;
+    }
+
+    public String getGitTagsUrl() {
+        return gitTagsUrl;
+    }
+
+    @JsonProperty("git_tags_url")
+    public void setGitTagsUrl(String gitTagsUrl) {
+        this.gitTagsUrl = gitTagsUrl;
+    }
+
+    public String getGitRefsUrl() {
+        return gitRefsUrl;
+    }
+
+    @JsonProperty("git_refs_url")
+    public void setGitRefsUrl(String gitRefsUrl) {
+        this.gitRefsUrl = gitRefsUrl;
+    }
+
+    public String getTreesUrl() {
+        return treesUrl;
+    }
+
+    @JsonProperty("trees_url")
+    public void setTreesUrl(String treesUrl) {
+        this.treesUrl = treesUrl;
+    }
+
+    public String getStatusesUrl() {
+        return statusesUrl;
+    }
+
+    @JsonProperty("statuses_url")
+    public void setStatusesUrl(String statusesUrl) {
+        this.statusesUrl = statusesUrl;
+    }
+
+    public String getLanguagesUrl() {
+        return languagesUrl;
+    }
+
+    @JsonProperty("languages_url")
+    public void setLanguagesUrl(String languagesUrl) {
+        this.languagesUrl = languagesUrl;
+    }
+
+    public String getStargazersUrl() {
+        return stargazersUrl;
+    }
+
+    @JsonProperty("stargazers_url")
+    public void setStargazersUrl(String stargazersUrl) {
+        this.stargazersUrl = stargazersUrl;
+    }
+
+    public String getContributorsUrl() {
+        return contributorsUrl;
+    }
+
+    @JsonProperty("contributors_url")
+    public void setContributorsUrl(String contributorsUrl) {
+        this.contributorsUrl = contributorsUrl;
+    }
+
+    public String getSubscribersUrl() {
+        return subscribersUrl;
+    }
+
+    @JsonProperty("subscribers_url")
+    public void setSubscribersUrl(String subscribersUrl) {
+        this.subscribersUrl = subscribersUrl;
+    }
+
+    public String getSubscriptionUrl() {
+        return subscriptionUrl;
+    }
+
+    @JsonProperty("subscription_url")
+    public void setSubscriptionUrl(String subscriptionUrl) {
+        this.subscriptionUrl = subscriptionUrl;
+    }
+
+    public String getCommitsUrl() {
+        return commitsUrl;
+    }
+
+    @JsonProperty("commits_url")
+    public void setCommitsUrl(String commitsUrl) {
+        this.commitsUrl = commitsUrl;
+    }
+
+    public String getGitCommitsUrl() {
+        return gitCommitsUrl;
+    }
+
+    @JsonProperty("git_commits_url")
+    public void setGitCommitsUrl(String gitCommitsUrl) {
+        this.gitCommitsUrl = gitCommitsUrl;
+    }
+
+    public String getCommentsUrl() {
+        return commentsUrl;
+    }
+
+    @JsonProperty("comments_url")
+    public void setCommentsUrl(String commentsUrl) {
+        this.commentsUrl = commentsUrl;
+    }
+
+    public String getIssueCommentUrl() {
+        return issueCommentUrl;
+    }
+
+    @JsonProperty("issue_comment_url")
+    public void setIssueCommentUrl(String issueCommentUrl) {
+        this.issueCommentUrl = issueCommentUrl;
+    }
+
+    public String getContentsUrl() {
+        return contentsUrl;
+    }
+
+    @JsonProperty("contents_url")
+    public void setContentsUrl(String contentsUrl) {
+        this.contentsUrl = contentsUrl;
+    }
+
+    public String getCompareUrl() {
+        return compareUrl;
+    }
+
+    @JsonProperty("compare_url")
+    public void setCompareUrl(String compareUrl) {
+        this.compareUrl = compareUrl;
+    }
+
+    public String getMergesUrl() {
+        return mergesUrl;
+    }
+
+    @JsonProperty("merges_url")
+    public void setMergesUrl(String mergesUrl) {
+        this.mergesUrl = mergesUrl;
+    }
+
+    public String getArchiveUrl() {
+        return archiveUrl;
+    }
+
+    @JsonProperty("archive_url")
+    public void setArchiveUrl(String archiveUrl) {
+        this.archiveUrl = archiveUrl;
+    }
+
+    public String getDownloadsUrl() {
+        return downloadsUrl;
+    }
+
+    @JsonProperty("downloads_url")
+    public void setDownloadsUrl(String downloadsUrl) {
+        this.downloadsUrl = downloadsUrl;
+    }
+
+    public String getIssuesUrl() {
+        return issuesUrl;
+    }
+
+    @JsonProperty("issues_url")
+    public void setIssuesUrl(String issuesUrl) {
+        this.issuesUrl = issuesUrl;
+    }
+
+    public String getPullsUrl() {
+        return pullsUrl;
+    }
+
+    @JsonProperty("pulls_url")
+    public void setPullsUrl(String pullsUrl) {
+        this.pullsUrl = pullsUrl;
+    }
+
+    public String getMilestonesUrl() {
+        return milestonesUrl;
+    }
+
+    @JsonProperty("milestones_url")
+    public void setMilestonesUrl(String milestonesUrl) {
+        this.milestonesUrl = milestonesUrl;
+    }
+
+    public String getNotificationsUrl() {
+        return notificationsUrl;
+    }
+
+    @JsonProperty("notifications_url")
+    public void setNotificationsUrl(String notificationsUrl) {
+        this.notificationsUrl = notificationsUrl;
+    }
+
+    public String getLabelsUrl() {
+        return labelsUrl;
+    }
+
+    @JsonProperty("labels_url")
+    public void setLabelsUrl(String labelsUrl) {
+        this.labelsUrl = labelsUrl;
+    }
+
+    public String getReleasesUrl() {
+        return releasesUrl;
+    }
+
+    @JsonProperty("releases_url")
+    public void setReleasesUrl(String releasesUrl) {
+        this.releasesUrl = releasesUrl;
+    }
+
+    public String getDeploymentsUrl() {
+        return deploymentsUrl;
+    }
+
+    @JsonProperty("deployments_url")
+    public void setDeploymentsUrl(String deploymentsUrl) {
+        this.deploymentsUrl = deploymentsUrl;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    @JsonProperty("created_at")
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @JsonProperty("updated_at")
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getPushedAt() {
+        return pushedAt;
+    }
+
+    @JsonProperty("pushed_at")
+    public void setPushedAt(String pushedAt) {
+        this.pushedAt = pushedAt;
+    }
+
+    public String getGitUrl() {
+        return gitUrl;
+    }
+
+    @JsonProperty("git_url")
+    public void setGitUrl(String gitUrl) {
+        this.gitUrl = gitUrl;
+    }
+
+    public String getSshUrl() {
+        return sshUrl;
+    }
+
+    @JsonProperty("ssh_url")
+    public void setSshUrl(String sshUrl) {
+        this.sshUrl = sshUrl;
+    }
+
+    public String getCloneUrl() {
+        return cloneUrl;
+    }
+
+    @JsonProperty("clone_url")
+    public void setCloneUrl(String cloneUrl) {
+        this.cloneUrl = cloneUrl;
+    }
+
+    public String getSvnUrl() {
+        return svnUrl;
+    }
+
+    @JsonProperty("svn_url")
+    public void setSvnUrl(String svnUrl) {
+        this.svnUrl = svnUrl;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    @JsonProperty("homepage")
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    @JsonProperty("size")
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getStargazersCount() {
+        return stargazersCount;
+    }
+
+    @JsonProperty("stargazers_count")
+    public void setStargazersCount(Integer stargazersCount) {
+        this.stargazersCount = stargazersCount;
+    }
+
+    public Integer getWatchersCount() {
+        return watchersCount;
+    }
+
+    @JsonProperty("watchers_count")
+    public void setWatchersCount(Integer watchersCount) {
+        this.watchersCount = watchersCount;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    @JsonProperty("language")
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Boolean getHasIssues() {
+        return hasIssues;
+    }
+
+    @JsonProperty("has_issues")
+    public void setHasIssues(Boolean hasIssues) {
+        this.hasIssues = hasIssues;
+    }
+
+    public Boolean getHasProjects() {
+        return hasProjects;
+    }
+
+    @JsonProperty("has_projects")
+    public void setHasProjects(Boolean hasProjects) {
+        this.hasProjects = hasProjects;
+    }
+
+    public Boolean getHasDownloads() {
+        return hasDownloads;
+    }
+
+    @JsonProperty("has_downloads")
+    public void setHasDownloads(Boolean hasDownloads) {
+        this.hasDownloads = hasDownloads;
+    }
+
+    public Boolean getHasWiki() {
+        return hasWiki;
+    }
+
+    @JsonProperty("has_wiki")
+    public void setHasWiki(Boolean hasWiki) {
+        this.hasWiki = hasWiki;
+    }
+
+    public Boolean getHasPages() {
+        return hasPages;
+    }
+
+    @JsonProperty("has_pages")
+    public void setHasPages(Boolean hasPages) {
+        this.hasPages = hasPages;
+    }
+
+    public Integer getForksCount() {
+        return forksCount;
+    }
+
+    @JsonProperty("forks_count")
+    public void setForksCount(Integer forksCount) {
+        this.forksCount = forksCount;
+    }
+
+    public Object getMirrorUrl() {
+        return mirrorUrl;
+    }
+
+    @JsonProperty("mirror_url")
+    public void setMirrorUrl(Object mirrorUrl) {
+        this.mirrorUrl = mirrorUrl;
+    }
+
+    public Boolean getArchived() {
+        return archived;
+    }
+
+    @JsonProperty("archived")
+    public void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
+
+    public Boolean getDisabled() {
+        return disabled;
+    }
+
+    @JsonProperty("disabled")
+    public void setDisabled(Boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    public Integer getOpenIssuesCount() {
+        return openIssuesCount;
+    }
+
+    @JsonProperty("open_issues_count")
+    public void setOpenIssuesCount(Integer openIssuesCount) {
+        this.openIssuesCount = openIssuesCount;
+    }
+
+    public License getLicense() {
+        return license;
+    }
+
+    @JsonProperty("license")
+    public void setLicense(License license) {
+        this.license = license;
+    }
+
+    public Integer getForks() {
+        return forks;
+    }
+
+    @JsonProperty("forks")
+    public void setForks(Integer forks) {
+        this.forks = forks;
+    }
+
+    public Integer getOpenIssues() {
+        return openIssues;
+    }
+
+    @JsonProperty("open_issues")
+    public void setOpenIssues(Integer openIssues) {
+        this.openIssues = openIssues;
+    }
+
+    public Integer getWatchers() {
+        return watchers;
+    }
+
+    @JsonProperty("watchers")
+    public void setWatchers(Integer watchers) {
+        this.watchers = watchers;
+    }
+
+    public String getDefaultBranch() {
+        return defaultBranch;
+    }
+
+    @JsonProperty("default_branch")
+    public void setDefaultBranch(String defaultBranch) {
+        this.defaultBranch = defaultBranch;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    @JsonProperty("score")
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    //public Map<String, Object> getAdditionalProperties() {
+       // return this.additionalProperties;
+    //}
+
+    //public void setAdditionalProperty(String name, Object value) {
+      //  this.additionalProperties.put(name, value);
+    //}
+
+}
---
+++ src/main/java/com/arthurezeagbo/repoinfoservice/models/TrendingRepo.java
@@ -0,0 +1,50 @@
+package com.arthurezeagbo.repoinfoservice.models;
+
+import com.arthurezeagbo.repoinfoservice.models.Item;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TrendingRepo {
+
+    private Integer totalCount;
+    private Boolean incompleteResults;
+    private List<Item> items = null;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public TrendingRepo(){}
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public Boolean getIncompleteResults() {
+        return incompleteResults;
+    }
+
+    public void setIncompleteResults(Boolean incompleteResults) {
+        this.incompleteResults = incompleteResults;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
